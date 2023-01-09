f1a0e9a38f0705c092ba37078deaa76b763de6d3
Ujjwal Kumar <113303058+Ujjwal92425@users.noreply.github.com>
2023-01-09 09:51:40 +0530

    new updated

---
+++ forLOOP.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+    int a;
+    for (a = 0; a < 100; a++)
+    {
+        printf(" \t Jaymod Singh \t  Mamta Singh \t Ujjwal Kumar \t Aditya Kumar \n");
+        printf("  Jaymod Singh \t  Mamta Singh \t Ujjwal Kumar \t Aditya Kumar \n");
+        printf(" \tJaymod Singh \t  Mamta Singh \t Ujjwal Kumar \t Aditya Kumar \n");
+        printf("  Jaymod Singh \t  Mamta Singh \t Ujjwal Kumar \t Aditya Kumar \n");
+        printf("Jaymod Singh \t  Mamta Singh \t Ujjwal Kumar \t Aditya Kumar \n");
+    }
+    return 0;
+    
+}
