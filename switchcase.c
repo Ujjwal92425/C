ce64aec4182c3a9dc4ce91ab0ea5fe9681d7cda3
Ujjwal Kumar <113303058+Ujjwal92425@users.noreply.github.com>
2023-02-07 16:51:48 +0530

    update 7 feb

--- switchcase.c
+++ switchcase.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
     int number;
@@ -7,7 +7,7 @@ int main()
     scanf("%d",&number);
 
     printf("The number you have gussed is %d\n",number);
-    
+
     switch (number)
     {
     case 7:
@@ -16,6 +16,41 @@ int main()
 
     default:
         printf(" < WRONG GUESS > ^^>>__ BETTER LUCK NEXT TIME ");
+        break;
+    }
+    return 0;
+}*/
+
+#include <stdio.h>
+int main()
+{
+    int a, b, x;
+    printf("----- * Choose * Your * Number * -----\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("1 for sum\t 2 for subtraction\t3 for multiplication\t4 for division\n");
+    scanf("%d", &x);
+
+    switch (x)
+    {
+    case 1:
+        printf("Sum  : %d", a + b);
+        break;
+
+    case 2:
+        printf("Subtraction: %d ", a - b);
+        break;
+
+    case 3:
+        printf("Multiplication : %d", a * b);
+        break;
+
+    case 4:
+        printf("Division : %d", a / b);
+        break;
+
+    default:
+        printf("\nInvalid Input ");
         break;
     }
     return 0;
